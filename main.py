82ac90349640628b259ed5f3810d5e8622d0e37f
Niitto <nickpsalles@gmail.com>
2022-11-10 00:06:25 -0300

    falta completar dicionario

--- main.py
+++ main.py
@@ -1,38 +1,56 @@
 import sys
+import os as my_dir
 
 def main():
-    file = open("caso1.txt", "r")
+    # my_dir.chdir('../TrabalhoDois')
+    # for f in my_dir.listdir():    
+    #     print(f)
+    my_graph = {}
+    with open('../TrabalhoDois/caso1.txt', 'r') as file:
+        
+        f = file.readlines()
+
+        # while r != None:
+        #     print(r, end = '')
+        #     r = file.readline()
+
+
     
-    for()
+    for line in f:
 
-class Graph:
-    graph = {}
+        words = line.replace('\n', '').split(' -> ')
+        print(words)
+        a = words[0]
+        b = words[1]
+        print(a,b)
 
-    def build_graph(graph):
-        nodes = []
+#class Graph:
+#    graph = {}
+#
+#    def build_graph(graph):
+#        nodes = []
+
+main()
 
 
-
-
-
-class Solution:
-    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
-        def dfs(root, targetSum, path):
-            if root == None: return None
+# class Solution:
+#     def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+#         def dfs(root, targetSum, path):
+#             if root == None: return None
             
-            targetSum -= root.val
-            path.append(root.val)
+#             targetSum -= root.val
+#             path.append(root.val)
             
-            if root.left == None and root.right == None:
-                if targetSum == 0:
-                    ans.append(path.copy())
+#             if root.left == None and root.right == None:
+#                 if targetSum == 0:
+#                     ans.append(path.copy())
                 
-            else:
-                dfs(root.left, targetSum, path)
-                dfs(root.right, targetSum, path)
+#             else:
+#                 dfs(root.left, targetSum, path)
+#                 dfs(root.right, targetSum, path)
             
-            path.pop()
+#             path.pop()
             
-        ans = []    
-        dfs(root, targetSum, [])
-        return ans
+#         ans = []    
+#         dfs(root, targetSum, [])
+#         return ans
